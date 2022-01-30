3f6a57531a7f4e4ec360673c8f2bc95ee46cd6ae
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2022-01-30 06:01:00 +0100

    feat: enum query param & docs for /sbox/

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,35 @@ extern crate dotenv;
 pub mod models;
 pub mod schema;
 
+use rocket::form::FromFormField;
 use rocket::serde::json::Json;
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::okapi::schemars::JsonSchema;
 use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
 
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
+#[derive(Debug, FromFormField, JsonSchema)]
+enum Language {
+    Javascript,
+    Python,
+}
+
 #[openapi]
-#[post("/source")]
-fn new_source() -> Json<models::Source> {
+#[get("/sbox?<language>")]
+fn get_sbox(language: Language) {
+    println!("{:?}", language);
+    /*
+    Lang should be an enum - make show in docs
+    Return a sbox-wasm module
+    */
+}
+
+#[openapi]
+#[get("/source/<id>")]
+fn get_source(id: i32) -> Json<models::Source> {
     Json(models::Source {
         id: 1,
         lang: "lang",
@@ -25,8 +44,8 @@ fn new_source() -> Json<models::Source> {
 }
 
 #[openapi]
-#[get("/source/<id>")]
-fn get_source(id: i32) -> Json<models::Source> {
+#[post("/source")]
+fn new_source() -> Json<models::Source> {
     Json(models::Source {
         id: 1,
         lang: "lang",
@@ -53,10 +72,16 @@ fn rocket() -> _ {
     rocket::build()
         .mount(
             "/",
-            openapi_get_routes![new_source, get_source, update_source, delete_source],
+            openapi_get_routes![
+                get_sbox,
+                get_source,
+                new_source,
+                update_source,
+                delete_source
+            ],
         )
         .mount(
-            "/docs",
+            "/",
             make_swagger_ui(&SwaggerUIConfig {
                 url: "../openapi.json".to_owned(),
                 ..Default::default()
