acccbc926401cacf267a5a01ec6f678df83b3e82
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2022-01-30 07:47:41 +0100

    feat: with basic error handling

--- src/main.rs
+++ src/main.rs
@@ -8,60 +8,82 @@ pub mod models;
 pub mod schema;
 
 use rocket::form::FromFormField;
+use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket_okapi::okapi::schemars;
 use rocket_okapi::okapi::schemars::JsonSchema;
 use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
 
 use diesel::prelude::*;
+use diesel::result::Error;
 use dotenv::dotenv;
-use std::env;
+use std::error::Error as StdError;
+use std::{env, fmt};
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+#[derive(Debug)]
+pub enum ApiError {
+    NotFound,
+    InternalServerError,
+}
+
+impl fmt::Display for ApiError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ApiError::NotFound => f.write_str("NotFound"),
+            ApiError::InternalServerError => f.write_str("InternalServerError"),
+        }
+    }
+}
+
+impl StdError for ApiError {
+    fn description(&self) -> &str {
+        match *self {
+            ApiError::NotFound => "Not found",
+            ApiError::InternalServerError => "Internal server error",
+        }
+    }
+}
 
 #[derive(Debug, FromFormField, JsonSchema)]
 enum Language {
     Javascript,
-    Python,
 }
 
 #[openapi]
 #[get("/sbox?<language>")]
 fn get_sbox(language: Language) {
     println!("{:?}", language);
-    /*
-    Lang should be an enum - make show in docs
-    Return a sbox-wasm module
-    */
+    /* Return a sbox-wasm module */
 }
 
 #[openapi]
 #[get("/source/<id>")]
-fn get_source(id: i32) -> Json<models::Source> {
-    Json(models::Source {
-        id: 1,
-        lang: "lang",
-        src: "src",
-    })
+fn get_source(id: i32) -> Result<Json<models::Source>, rocket::http::Status> {
+    use self::schema::source::dsl::*;
+    let connection = establish_connection();
+    let s = source.find(id).first::<models::Source>(&connection);
+    match s {
+        Ok(s) => Ok(Json(s)),
+        Err(err) => match err {
+            _ => Err(Status::NotFound),
+        },
+    }
 }
 
 #[openapi]
 #[post("/source")]
-fn new_source() -> Json<models::Source> {
-    Json(models::Source {
-        id: 1,
-        lang: "lang",
-        src: "src",
-    })
-}
+fn new_source() {}
 
 #[openapi]
 #[put("/source/<id>")]
-fn update_source(id: i32) -> Json<models::Source> {
-    Json(models::Source {
-        id: 1,
-        lang: "lang",
-        src: "src",
-    })
-}
+fn update_source(id: i32) {}
 
 #[openapi]
 #[delete("/source/<id>")]
--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,13 @@
+use rocket::serde::json::Json;
 use rocket_okapi::okapi::schemars;
 use rocket_okapi::okapi::schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
-#[derive(Queryable, Deserialize, Serialize, JsonSchema)]
+#[derive(Queryable, Deserialize, Serialize, JsonSchema, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Source {
     pub id: i32,
-    pub lang: &'static str,
-    pub src: &'static str,
+    pub lang: String,
+    pub src: String,
 }
