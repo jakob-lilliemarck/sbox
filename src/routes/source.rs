a510f426422a46b8cf0b29088a624c9e415f4ea8
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2022-01-31 11:40:38 +0100

    WIP - issue with conn ref type

--- src/routes/source.rs
+++ src/routes/source.rs
@@ -1,12 +1,35 @@
+use crate::db::test::Conn;
 use crate::models::source::Source;
+use crate::schema;
 use diesel::prelude::*;
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket_okapi::openapi;
+use rocket_sync_db_pools::diesel;
+
+pub fn find(conn: &PgConnection, id: i32) -> Option<Source> {
+    schema::source::table
+        .find(id)
+        .get_result(conn)
+        .map_err(|err| println!("find_user: {}", err))
+        .ok()
+}
 
 #[openapi(tag = "Source")]
 #[get("/source/<id>")]
-pub fn read_source(id: i32) {}
+pub async fn read_source(conn: Conn, id: i32) /*-> Result<Json<Source>, Status>*/
+{
+    let s = find(&conn, id);
+    println!("{:?}", s)
+
+    /*let s = source.find(7).first::<Source>(&conn);
+    match s {
+        Ok(s) => Ok(Json(s)),
+        Err(err) => match err {
+            _ => Err(Status::NotFound),
+        },
+    }*/
+}
 
 #[openapi(tag = "Source")]
 #[post("/source")]
