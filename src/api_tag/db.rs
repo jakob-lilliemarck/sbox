8e69cbe86f1a1742fdca6501f366fdb3a653b88e
Jakob Lilliemarck <lilliemarckjakob@gmail.com>
2022-02-03 17:29:05 +0100

    WIP - actix error handling

--- src/api_tag/db.rs
+++ src/api_tag/db.rs
@@ -4,13 +4,26 @@ use crate::models::Tag;
 use sbox::schema::tag;
 
 use diesel::prelude::*;
-use diesel::result::Error;
+use diesel::result::{DatabaseErrorKind, Error};
 use rocket_sync_db_pools::diesel;
 
 pub fn create(conn: &diesel::PgConnection, new_tag: &Tag) -> Result<Tag, Error> {
     diesel::insert_into(tag::table)
         .values(new_tag)
         .get_result::<Tag>(conn)
+}
+
+pub fn create_if_none(conn: &diesel::PgConnection, tag: &Tag) -> Result<Tag, Error> {
+    match diesel::insert_into(tag::table)
+        .values(tag)
+        .get_result::<Tag>(conn)
+    {
+        Ok(tag) => Ok(tag),
+        Err(err) => match err {
+            Error::DatabaseError(DatabaseErrorKind::UniqueViolation, _) => read(conn, &tag.id),
+            _ => Err(err),
+        },
+    }
 }
 
 pub fn read(conn: &diesel::PgConnection, tag_id: &String) -> Result<Tag, Error> {
--- src/api_tag/routes.rs
+++ src/api_tag/routes.rs
@@ -1,26 +1,85 @@
+extern crate futures;
+extern crate serde;
+
 use crate::db;
 use crate::models::Tag;
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{
+    dev::HttpResponseBuilder, error, get, http::header, http::StatusCode, post, web, Error,
+    HttpRequest, HttpResponse, Responder,
+};
+use derive_more::{Display, Error};
+use futures::future::{ready, Ready};
 
 type DbPool = r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::PgConnection>>;
 
-#[post("/tags")]
-pub async fn create_tag(pool: web::Data<DbPool>) -> impl Responder {
-    let conn = pool.get().expect("Could not connect to db from pool.");
-    let new_tag = Tag {
-        id: "TEST".to_string(),
-    };
-    println!("TESTTEST");
-    let res = db::create(&conn, &new_tag);
-    println!("CREATED: {:?}", res);
-    HttpResponse::Ok().body("OKOK!")
+/* ERRORHANDLING WIP */
+#[derive(Debug, Display)]
+pub enum ServerError {
+    #[display(fmt = "internal error")]
+    InternalError,
+    #[display(fmt = "not found")]
+    NotFound,
 }
 
+impl actix_web::error::ResponseError for ServerError {
+    fn error_response(&self) -> HttpResponse {
+        println!("{:?}", self);
+        match self {
+            ServerError::InternalError => HttpResponse::InternalServerError().json("Server error."),
+            ServerError::NotFound => HttpResponse::NotFound().json("Not found"),
+        }
+    }
+}
+
+impl From<diesel::result::Error> for ServerError {
+    fn from(err: diesel::result::Error) -> ServerError {
+        match err {
+            diesel::result::Error::NotFound => ServerError::NotFound,
+            _ => ServerError::InternalError,
+        }
+    }
+}
+/* ENDOF ERRORHANDLING WIP */
+
 #[get("/tags/{id}")]
-pub async fn get_tags(pool: web::Data<DbPool>, id: web::Path<(String)>) -> impl Responder {
-    println!("RECIEVED ID: {:?}", id);
-    let conn = pool.get().expect("Could not connect to db from pool.");
-    let res = db::read(&conn, &id);
-    println!("FOUND: {:?}", res);
-    HttpResponse::Ok().body("OK!")
+pub async fn get_tags(
+    pool: web::Data<DbPool>,
+    id: web::Path<String>,
+) -> Result<HttpResponse, ServerError> {
+    match db::read(
+        &pool.get().expect("Could not connect to db from pool."),
+        &id,
+    ) {
+        Ok(tag) => Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(serde_json::to_string(&tag).expect("json serialization error"))),
+        Err(err) => {
+            println!("ERR: {:?}", err);
+            Err(err.into())
+        }
+    }
+}
+
+impl Responder for Tag {
+    type Error = ServerError;
+    type Future = Ready<Result<HttpResponse, ServerError>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        println!("BEFORE BODY SERIALIZATION");
+        let body = serde_json::to_string(&self).unwrap();
+        // Create response and set content typex§
+        ready(Ok(HttpResponse::Ok()
+            // TODO - common header shouled be set centrally.
+            .content_type("application/json")
+            .body("HEJ")))
+    }
+}
+
+#[post("/tags")]
+pub async fn create_tag(pool: web::Data<DbPool>, tag: web::Json<Tag>) -> impl Responder {
+    db::create_if_none(
+        &pool.get().expect("Could not connect to db from pool."),
+        &tag,
+    )
+    .unwrap()
 }
